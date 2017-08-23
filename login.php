6293a2fafa701531d2c58ed5bb7f2547c2297ce6
punit121 <punit.kumar1661@gmail.com>
2017-08-24 03:51:55 +0530

    code added

---
+++ login.php
@@ -0,0 +1,39 @@
+
+<head>
+	<link href="css/bootstrap-responsive.css" rel="stylesheet">
+<link href="css/style.css" rel="stylesheet">
+<link href="css/login.css" rel="stylesheet">
+<link href="color/default.css" rel="stylesheet">
+<link rel="shortcut icon" href="img/favicon.ico">
+
+  </head>
+<?php include 'navbar.php'; ?>
+
+<div class="" style="margin-top:40px;">
+
+    <div class="login-wrapper">
+      
+      <h2 >Login Here </h2>
+      
+      <form action="" method="post">
+        
+        <div class="row">
+          <input type="email" name="email" class="login-input" placeholder="Email" required />
+        </div>
+        
+        <div class="row">
+          <input type="password" name="password" class="login-input" placeholder="Password" required />
+          
+        </div>
+    
+        <div class="row">
+          <button type="submit" class="submit-btn" name="submit" >Login </button>
+        
+        </div>
+      
+      </form>
+    
+    </div>
+
+</div>
+<?php include 'footer.php'; ?>
